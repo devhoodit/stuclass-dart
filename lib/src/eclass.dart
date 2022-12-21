e9ca457468861ebc1674602c164adee234b73a30
devhoodit <72541438+devhoodit@users.noreply.github.com>
2022-12-21 21:39:25 +0900

    feature: add auth & leacture material

--- lib/src/eclass.dart
+++ lib/src/eclass.dart
@@ -1,14 +1,19 @@
+import 'package:stuclass/src/lecture_material.dart';
+
 import 'login.dart';
 import 'subject.dart';
 import 'attend.dart';
 import 'base.dart';
+import 'auth.dart';
 
 class Eclass {
   late String _id;
   final BaseInfo _baseInfo = BaseInfo('', '', '');
   late Login login;
+  late Auth auth;
   late Subject subject;
   late Attend attend;
+  late LectureMaterial lectureMaterial;
 
   Eclass(String id, String password) {
     _id = id;
@@ -20,7 +25,10 @@ class Eclass {
     if (baseinfo['LMS_SESSIONID'] == '') throw 'id or password not matched';
     _baseInfo(_id, baseinfo['LMS_SESSIONID']!, baseinfo['WMONID']!);
 
-    subject = Subject(_baseInfo);
-    attend = Attend(_baseInfo, subject);
+    auth = Auth(_baseInfo);
+
+    subject = Subject(_baseInfo, auth);
+    attend = Attend(_baseInfo, auth, subject);
+    lectureMaterial = LectureMaterial(_baseInfo, auth);
   }
 }
